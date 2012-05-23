76d9ece600d85bbbcb5c5d7e7916865dad55b08f
Dan Pupius <dan@obvious.com>
2012-05-23 15:52:28 -0700

    Initial implementation of soynode.
    Supports compilation of soy files and loading of soy.js files.
    Compilation is delegated to the SoyToJsSrcCompiler which is
    executed via a system call to java.
    
    Support for dynamic recompilation is available and temporary
    files can be cleaned up after successful compilations.
    
    This check-in includes a copy of Soy, downloaded on 22/05/12.

---
+++ examples/errors.soy.js
@@ -0,0 +1,15 @@
+// This file was automatically generated from errors.soy.
+// Please don't edit this file by hand.
+
+if (typeof example == 'undefined') { var example = {}; }
+if (typeof example.errors == 'undefined') { example.errors = {}; }
+
+
+example.errors.error404 = function(opt_data) {
+  return '<h1>Not Found!</h1>';
+};
+
+
+example.errors.error500 = function(opt_data) {
+  return '<h1>Oops, my bad</h1>';
+};
